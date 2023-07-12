d324164b8caf5027529a27092696e39f33291d35
oc34n <ocean.wrng@gmail.com>
2023-07-11 20:58:11 -0400

    User can now style QR code
    >Download persist QRCode styling
    >Removed strictmode from main.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,45 @@
-import React, {useState} from 'react';
-import QRCode from 'react-qr-code';
-import './App.css';
-import ColorPicker from './ColorPicker';
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
+import "./App.css";
 
 export default function App() {
-  const [qrVal, setQrVal] = useState(''); 
-  const [generatedQrVal, setGenQrVal] = useState('');
+  const [qrVal, setQrVal] = useState("");
+  const [generatedQrVal, setGenQrVal] = useState("");
   const [showQr, setShowQr] = useState(false);
+  const [fgColor, setFgColor] = useState("#000000");
+  const [bgColor, setBgColor] = useState("#ffffff");
 
-  function handleChange(e){
-    setQrVal(e.target.value)
+  //set state value for qr code
+  function handleChange(e) {
+    setQrVal(e.target.value);
   }
 
-  function handleKeyPress(e){
-    e.key === 'Enter' && generateQr();
+  //Keyboard Accessibility
+  function handleKeyPress(e) {
+    e.key === "Enter" && generateQr();
+  }
+  function handleKeyDl(e) {
+    e.key === "Enter" && downloadQR();
   }
 
-  function generateQr(){
-    qrVal ? (
-      setShowQr(true),
-      setGenQrVal(qrVal)
-    ) : alert('Please Enter your text or URL of choice!');
+  //changes styling state for QR code
+  function handleFgVal(e) {
+    setFgColor(e.target.value);
+  }
+  function handleBgVal(e) {
+    setBgColor(e.target.value);
   }
 
-// converts the QRCode SVG element to an image by rendering it onto a canvas, 
-//converting the canvas content to a PNG Data URL, 
-//and then triggering the download of the resulting image.
+  //if input field has a value generate new QR
+  function generateQr() {
+    qrVal
+      ? (setShowQr(true), setGenQrVal(qrVal))
+      : alert("Please Enter your text or URL of choice!");
+  }
+
+  // converts the QRCode SVG element to an image by rendering it onto a canvas,
+  //converting the canvas content to a PNG Data URL,
+  //and then triggering the download of the resulting image.
   function downloadQR() {
     const svg = document.getElementById("QRCode");
     const svgData = new XMLSerializer().serializeToString(svg);
@@ -43,21 +57,70 @@ export default function App() {
       downloadLink.click();
     };
     img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
-  };
+  }
 
   return (
     <main>
       <section>
-        {showQr ? <ColorPicker/> : <h1>Enter a URL to Generate Your QR Code!</h1>}
-        <div className='qrCode'>
-          {showQr ? <QRCode id='QRCode' size={200} bgColor='white' fgColor='black' value={generatedQrVal}/> : <h2>QR Box</h2>}
+        {showQr ? (
+          <section className="colorPicker">
+            <div className="fg-box">
+              <p>Foreground Color</p>
+              <input
+                id="fg-color"
+                type="color"
+                value={fgColor}
+                onChange={handleFgVal}
+              />
+            </div>
+            <div className="bg-box">
+              <p>Background Color</p>
+              <input
+                id="bg-color"
+                type="color"
+                value={bgColor}
+                onChange={handleBgVal}
+              />
+            </div>
+          </section>
+        ) : (
+          <h1>Enter a URL to Generate Your QR Code!</h1>
+        )}
+        <div className="qrCode">
+          {showQr ? (
+            <QRCode
+              id="QRCode"
+              size={200}
+              fgColor={fgColor}
+              bgColor={bgColor}
+              value={generatedQrVal}
+            />
+          ) : (
+            <h2>QR Box</h2>
+          )}
         </div>
-        {showQr && <button className='dwnlBtn' onClick={downloadQR}>Download QR</button>}
-        <div className="genQr" style={{display:'flex', alignItems:'center'}}>
-          <input type='text' placeholder='Your url or text..' onChange={handleChange} onKeyDown={handleKeyPress}/>
-          <button onClick={generateQr}>Generate</button>
+        {showQr && (
+          <button
+            className="dwnlBtn"
+            onClick={downloadQR}
+            onKeyDown={handleKeyDl}
+          >
+            Download QR
+          </button>
+        )}
+        <div
+          className="genQr"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <input
+            type="text"
+            placeholder="Your url or text.."
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+          />
+          <button onClick={generateQr}>Generate!</button>
         </div>
       </section>
     </main>
-  )
+  );
 }
